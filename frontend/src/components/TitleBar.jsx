d668a755eec6b8b13498f7283d34fb696b3a0e42
your.user.name <jundeptrainhat@gmail.com>
2024-06-05 19:45:59 +0700

    hello

---
+++ frontend/src/components/TitleBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const TitleBar = () => {
+  return (
+    <>
+      <div className="w-full mx-20">
+        <h1 className="">TITLE CATEGORY</h1>
+        <a>Detail category Detail category Detail category</a>
+      </div>
+    </>
+  );
+};
+
+export default TitleBar;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,19 @@
+import Categorybar from "@/components/Categorybar";
 import FilterHome from "@/components/FilterHome";
 import SearchBanner from "@/components/SearchBanner";
+import TitleBar from "@/components/TitleBar";
 
 export const HomePage = () => {
   return (
     <>
       <SearchBanner></SearchBanner>
       <FilterHome></FilterHome>
+      <TitleBar></TitleBar>
+      <Categorybar></Categorybar>
+      <TitleBar></TitleBar>
+      <Categorybar></Categorybar>
+      <TitleBar></TitleBar>
+      <Categorybar></Categorybar>
     </>
   );
 };
