b3cda9177c73104a4fbb44a9634e42c56b0b8cf8
your.user.name <jundeptrainhat@gmail.com>
2024-06-09 21:06:57 +0700

    update color search button

--- frontend/src/components/ui/Searchbutton.jsx
+++ frontend/src/components/ui/Searchbutton.jsx
@@ -8,7 +8,7 @@ const searchButtonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-red-500 text-white hover:bg-red-600",
+        default: "bg-red-600 text-white hover:bg-red-600",
         destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
